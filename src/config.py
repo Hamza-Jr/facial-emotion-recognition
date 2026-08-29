8d3976e28efff41633b8cff9887fd5ad1ecb0d9c
Hamza-Jr <92399369+Hamza-Jr@users.noreply.github.com>
2026-08-29 13:41:57 +0100

    chore: add project configuration

---
+++ src/config.py
@@ -0,0 +1,18 @@
+# Image configuration
+IMAGE_SIZE = (48, 48)
+IMAGE_CHANNELS = 1
+NORMALIZATION_FACTOR = 255.0
+
+NUM_CLASSES = 7
+
+# # Emotion label mapping classes
+EMOTION_LABELS = {
+    0: "Anger",
+    1: "Disgust",
+    2: "Fear",
+    3: "Happy",
+    4: "Sad",
+    5: "Surprise",
+    6: "Neutral",
+}
+
